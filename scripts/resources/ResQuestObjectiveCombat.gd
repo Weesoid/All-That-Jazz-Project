3187127c9b5269f53eb54260a122fc4bbe70faf1
Weesoid <133038580+Weesoid@users.noreply.github.com>
2023-08-09 21:32:44 +0800

    Combat Objective Adjustment
    > You can now set how many times the player should beat the unique ID fight to complete the objective

--- scripts/resources/ResQuestObjectiveCombat.gd
+++ scripts/resources/ResQuestObjectiveCombat.gd
@@ -3,8 +3,10 @@ class_name ResQuestObjectiveCombat
 
 @export var UNIQUE_COMBAT_ID: String
 @export var REQUIRE_WIN: bool = true
+@export var COUNT: int = 1
 
 var id
+var objective_count = 0
 var combat_result
 
 func initializeObjective():
@@ -24,8 +26,10 @@ func initializeObjective():
 func checkComplete():
 	if UNIQUE_COMBAT_ID == id:
 		if REQUIRE_WIN and combat_result == 1:
-			FINISHED = true
-			PlayerGlobals.quest_objective_completed.emit()
+			objective_count += 1
 		elif !REQUIRE_WIN:
-			FINISHED = true
-			PlayerGlobals.quest_objective_completed.emit()
+			objective_count += 1
+	
+	if objective_count == COUNT:
+		FINISHED = true
+		PlayerGlobals.quest_objective_completed.emit()
