cc8f27d18d86b13139ca5c3bb04648225b447822
Weesoid <133038580+Weesoid@users.noreply.github.com>
2023-08-12 18:46:51 +0800

    Added basic 'Singed' status effect
    > Yep.

--- scripts/abilities/scsSinged.gd
+++ scripts/abilities/scsSinged.gd
@@ -1,5 +1,8 @@
 static func applyEffects(target: ResCombatant, status_effect: ResStatusEffect):
 	if status_effect.APPLY_ONCE:
+		var damage = target.BASE_STAT_VALUES['health'] * 0.1
+		CombatGlobals.manual_call_indicator.emit(target, str(int(damage), ' SINGED!'), 'Show')
+		target.STAT_VALUES['health'] -= int(damage)
 		CombatGlobals.modifyStat(target, 'heal mult', -0.75)
 
 static func endEffects(target: ResCombatant):
